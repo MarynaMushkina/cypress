c152faba26ffdaca6439b7d5f60bb15e37790d56
Maryna Mushkina <mmushkina@ek-mb-008.local>
2022-01-31 11:45:29 +0200

    HW_3

--- cypress/integration/allo.ua/test.spec.js
+++ cypress/integration/allo.ua/test.spec.js
@@ -1,10 +1,18 @@
-describe("My allo.ua", function() {
-    beforeEach(function() {
-        cy.viewport('macbook-13')
-      })
-      it("Open site", function() {
-        cy.visit("https://allo.ua/")
+describe('My allo.ua', () => {
+    it('Open site', () => {
+        cy.visit('/')
         cy.contains('Магазини').click()
         cy.get('.content__title').should('have.text','Адреси магазинів')
+
     })
+    it('Check the search field', () => {  
+        cy.get('input[id="search-form__input"]').type('xiaomi{enter}')
+        cy.get('.product-card').should('be.visible')
+        cy.get('.product-card').should('have.length', 28)
+    }) 
+    it('Check that active filter "Смартфони і мобільні телефони"', () => {  
+        cy.get('a[title="Смартфони і мобільні телефони"]').click()
+        cy.get('.filter-popup__button').click()
+        cy.get('.toolbar-block').find('.active-filter span').should('have.text','Смартфони і мобільні телефони')
+    }) 
 })
